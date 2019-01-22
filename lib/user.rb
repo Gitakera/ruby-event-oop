5a1db7e07aa54e6795efccde1db3861499de4bf3
Gitakera <myandopro@gmail.com>
2019-01-22 04:49:48 -0500

    modification user.rb

--- lib/user.rb
+++ lib/user.rb
@@ -2,13 +2,15 @@ class User
   attr_accessor :email, :name
   @@user_count = 0
   @@all = []
-  @@user_cree=[]
+  @@save_self=Hash.new
 
   def initialize(email_to_save)
     @email = email_to_save
     @@all[@@user_count]=email_to_save
+    #@@save_self << self
+    @@save_self.store(email_to_save,self)
     @@user_count = @@user_count + 1
-    @@instance[@@user_count]
+    
     puts "objet cree #{email_to_save}"
   end
 
@@ -20,15 +22,16 @@ class User
     return ObjectSpace.each_object(Class).select { |klass| klass < self }
   end
 	
-  def affiche_moi
-    return self
-  end
+
+
   def self.count
+    puts @@save_self
+    puts @@save_self["maila"]
     return @@user_count
   end
 
   def self.find_by_email(email)
-    puts email
+    
    
   end
 
